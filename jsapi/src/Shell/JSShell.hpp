8a2148d4467da2e4b6ce778848bc5df721f7c68a
玩游戏的李子 <liyuqili_1379zi@qq.com>
2025-12-20 15:49:16 +0800

    Add files via upload

--- jsapi/src/Shell/JSShell.hpp
+++ jsapi/src/Shell/JSShell.hpp
@@ -1,4 +1,3 @@
-// JSShell.hpp
 #pragma once
 
 #include "Shell.hpp"
@@ -13,32 +12,13 @@ class JSShell : public JQPublishObject
 private:
     std::unique_ptr<Shell> shell;
     std::mutex mutex;
-    
-    // 事件队列
-    struct Event {
-        std::string type;
-        std::string data;
-        int code{0};
-    };
-    std::vector<Event> eventQueue;
-    
+
 public:
     JSShell();
     ~JSShell();
-    
+
     void initialize(JQFunctionInfo& info);
     void exec(JQAsyncInfo& info);
-    
-    // 新增方法
-    void execAsync(JQAsyncInfo& info);
-    void startInteractive(JQAsyncInfo& info);
-    void writeInput(JQFunctionInfo& info);
-    void sendSignal(JQFunctionInfo& info);
-    void terminate(JQFunctionInfo& info);
-    void isRunning(JQFunctionInfo& info);
-    void setEnv(JQFunctionInfo& info);
-    void getExitCode(JQFunctionInfo& info);
-    
-    // 处理事件
-    void processEvents();
 };
+
+JSValue createShell(JQModuleEnv* env);
--- jsapi/src/Shell/Shell.hpp
+++ jsapi/src/Shell/Shell.hpp
@@ -1,60 +1,7 @@
-// Shell.hpp
 #pragma once
 #include <string>
-#include <functional>
-#include <memory>
-#include <vector>
-#include <atomic>
 
 class Shell {
 public:
-    // 回调函数类型定义
-    using OutputCallback = std::function<void(const std::string& output)>;
-    using ExitCallback = std::function<void(int exitCode)>;
-    using ErrorCallback = std::function<void(const std::string& error)>;
-    
-    // 构造和析构
-    Shell();
-    ~Shell();
-    
-    // 同步执行命令
     std::string exec(const std::string& cmd);
-    
-    // 异步执行命令（支持回显）
-    void execAsync(const std::string& cmd,
-                  OutputCallback outputCallback = nullptr,
-                  ExitCallback exitCallback = nullptr,
-                  ErrorCallback errorCallback = nullptr);
-    
-    // 执行多条命令序列
-    void execBatch(const std::vector<std::string>& cmds,
-                  OutputCallback outputCallback = nullptr,
-                  ExitCallback exitCallback = nullptr);
-    
-    // 启动交互式shell会话
-    void startInteractive(OutputCallback outputCallback,
-                         ExitCallback exitCallback = nullptr,
-                         ErrorCallback errorCallback = nullptr);
-    
-    // 向交互式会话写入输入
-    void writeToInteractive(const std::string& input);
-    
-    // 发送信号（如Ctrl+C, Ctrl+D）
-    void sendSignal(int signal);
-    
-    // 结束当前进程
-    void terminate();
-    
-    // 获取运行状态
-    bool isRunning() const;
-    
-    // 设置环境变量
-    void setEnv(const std::string& key, const std::string& value);
-    
-    // 获取最后执行的命令的退出码
-    int getLastExitCode() const;
-    
-private:
-    class Impl;
-    std::unique_ptr<Impl> pImpl;
 };
