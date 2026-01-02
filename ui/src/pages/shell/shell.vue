c6d333a5cdf4d036a12e64993d6a911134f5dfeb
玩游戏的李子 <liyuqili_1379zi@qq.com>
2026-01-02 23:55:49 +0800

    Update shell.vue

--- ui/src/pages/shell/shell.vue
+++ ui/src/pages/shell/shell.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container">
-    <!-- 终端输出区域 -->
     <div class="terminal-content">
       <scroller 
         class="terminal-scroller"
@@ -9,23 +8,18 @@
         :show-scrollbar="true"
       >
         <div v-for="line in terminalLines" :key="line.id" class="terminal-line">
-          <!-- 使用正确的类名语法 -->
-          <text class="line-text" :class="`line-text-${line.type}`">{{ line.content }}</text>
+          <!-- 使用简单直接的类名 -->
+          <text class="line-text" :class="'line-text-' + line.type">{{ line.content }}</text>
         </div>
         
-        <!-- 命令提示符 -->
         <div class="command-prompt">
           <text class="prompt">{{ currentDir }} $</text>
-          <!-- 修改光标闪烁实现 -->
-          <text v-if="!isExecuting" 
-                class="cursor" 
-                :style="{ opacity: cursorVisible ? 1 : 0 }">█</text>
+          <text v-if="!isExecuting" class="cursor" :style="{ opacity: cursorVisible ? 1 : 0 }">█</text>
           <text v-else class="loading">⌛ 执行中...</text>
         </div>
       </scroller>
     </div>
 
-    <!-- 输入区域 -->
     <div class="input-section">
       <div class="input-container" @click="openKeyboard">
         <text class="input-text">{{ inputText || '点击输入命令...' }}</text>
@@ -40,29 +34,27 @@
   </div>
 </template>
 
-<style lang="less" scoped>
-@import url('shell.less');
-</style>
-
 <script>
 import shell from './shell';
+
+// 正确扩展组件
 export default {
-  ...shell,
+  mixins: [shell],  // 使用 mixins 而不是展开操作符
+  
   data() {
     return {
-      ...(shell.data ? shell.data() : {}),
-      cursorVisible: true,
-      cursorInterval: null
+      cursorVisible: true
     };
   },
+  
   mounted() {
-    // 添加光标闪烁效果
+    // 光标闪烁
     this.cursorInterval = setInterval(() => {
       this.cursorVisible = !this.cursorVisible;
     }, 500);
   },
+  
   beforeUnmount() {
-    // 清理定时器
     if (this.cursorInterval) {
       clearInterval(this.cursorInterval);
     }
